0b85d1e73f1752d5e30492aef46f90c57920056e
Jan Hor <horj28@vse.cz>
2021-10-31 16:36:33 +0100

    Add Another Spring Animation

--- 07-build-custom-view-animations/starter/app/src/main/java/com/raywenderlich/android/menagerie/ui/feedPet/FeedPetActivity.kt
+++ 07-build-custom-view-animations/starter/app/src/main/java/com/raywenderlich/android/menagerie/ui/feedPet/FeedPetActivity.kt
@@ -1,10 +1,16 @@
 package com.raywenderlich.android.menagerie.ui.feedPet
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.dynamicanimation.animation.DynamicAnimation
+import androidx.dynamicanimation.animation.SpringAnimation
+import androidx.dynamicanimation.animation.SpringForce
 import coil.load
 import com.raywenderlich.android.menagerie.R
 import com.raywenderlich.android.menagerie.data.model.Pet
@@ -25,12 +31,23 @@ class FeedPetActivity : AppCompatActivity() {
   private val binding by lazy { ActivityFeedPetBinding.inflate(layoutInflater) }
   private val feedPetViewModel by viewModels<FeedPetViewModel>()
 
+  // animation
+  private var horizontalPositionDifference = 0f
+  private var vertikalPositionDifference = 0f
+  private val springForce by lazy {
+    SpringForce(0f).apply {
+      stiffness = SpringForce.STIFFNESS_MEDIUM
+      dampingRatio = SpringForce.DAMPING_RATIO_HIGH_BOUNCY
+    }
+  }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(binding.root)
     setupUi()
   }
 
+  @SuppressLint("ClickableViewAccessibility")
   private fun setupUi() {
     val data = intent.getSerializableExtra(KEY_PET) as? Pet ?: return
 
@@ -43,6 +60,58 @@ class FeedPetActivity : AppCompatActivity() {
       }
     })
 
-    // TODO physics animation
+    val springChocolateCookieX = SpringAnimation(
+      binding.chocolateCookie,
+      DynamicAnimation.TRANSLATION_X
+    ).setSpring(springForce)
+
+    val springCookieX = SpringAnimation(
+      binding.cookie,DynamicAnimation.TRANSLATION_X
+    ).setSpring(springForce)
+
+    val springChocolateCookieY = SpringAnimation(
+      binding.chocolateCookie,DynamicAnimation.TRANSLATION_Y
+    ).setSpring(springForce)
+
+    val springCookieY = SpringAnimation(
+      binding.cookie,DynamicAnimation.TRANSLATION_Y
+    ).setSpring(springForce)
+
+    binding.chocolateCookie.setOnTouchListener(buildTouchListener(
+      springChocolateCookieX,
+      springChocolateCookieY))
+
+    binding.cookie.setOnTouchListener(buildTouchListener(
+      springCookieX
+      , springCookieY))
+  }
+
+  @SuppressLint("ClickableViewAccessibility")
+  private fun buildTouchListener(
+    springAnimationX: SpringAnimation,
+    springAnimationY: SpringAnimation
+  ) =
+    View.OnTouchListener { view, event ->
+    when(event?.action) {
+      MotionEvent.ACTION_DOWN -> {
+        horizontalPositionDifference = event.rawX - (view?.x ?: 0f)
+        vertikalPositionDifference = event.rawY - (view?.y ?: 0f)
+
+        springAnimationX.cancel()
+        springAnimationY.cancel()
+      }
+
+      MotionEvent.ACTION_MOVE -> {
+        view.x = event.rawX - horizontalPositionDifference
+        view.y = event.rawY - vertikalPositionDifference
+      }
+
+      MotionEvent.ACTION_UP ->{
+        springAnimationX.start()
+        springAnimationY.start()
+      }
+    }
+
+    true
   }
 }
